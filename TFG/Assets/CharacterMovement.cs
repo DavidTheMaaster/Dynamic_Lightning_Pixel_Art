e565d1df2947dbb9f009dfddc8a13177ddef529d
David Lozano <david.lozano@innovamat.es>
2020-04-27 18:53:20 +0200

    Initial comit
    - Added repository
    - Created main character
    - Added Normal map to the first character

---
+++ TFG/Assets/CharacterMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("d"))
+        {
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f) * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += new Vector3(-1.0f, 0.0f, 0.0f) * speed * Time.deltaTime;
+        }
+    }
+}
